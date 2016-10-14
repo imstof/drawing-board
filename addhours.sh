7d4aa7acde4c3220ee3dca128cfdfe32e0f0f6ba
imstof <cehnstrom@techsquare.com>
2016-10-14 10:56:44 -0400

    Final revision (I hope)

--- addhours.sh
+++ addhours.sh
@@ -4,32 +4,35 @@
 sdate=$(date --date="last monday" +"%Y-%m-%d")
 edate=$(date +"%Y-%m-%d")
 
-#prompt for input
-echo ""
-echo "  Total hours worked on ENGAGING/C3DDB"
-echo "========================================"
-echo ""
 #check host. If aquinas use project/hours/ directory M-today, else prompt
 if [ $HOSTNAME = "planetaquinas" ]
 then
-	echo "You are on" $HOSTNAME
-	echo "You will search /projects/hours/$(date +"%Y")"
-	echo "Start: " $(date --date="last monday" +"%Y-%m-%d")
-	echo "End: " $(date +"%Y-%m-%d")
+#	echo "You are on" $HOSTNAME
+#	echo "You will search /projects/hours/$(date +"%Y")"
+#	echo "Start: " $sdate
+#	echo "End: " $edate
 	dir=/projects/hours/$(date +"%Y")/
+#	echo $dir  #test
 
-	for file in $dir*; do
+	for file in $dir$(date +"%Y")*; do
+#		echo $file  #test
 		if [ $(echo $file | cut -d'/' -f5 | cut -d'-' -f4) == "cehnstrom" ]
 		then
 		
-			fdate=$(echo $file | cut -d'/' -f7 | cut -d'-' -f1-3)
-			if [ $(echo $fdate) == $(echo $sdate) ] || [ $(echo $fdate) > $(echo $sdate) ]
+			fdate=$(echo $file | cut -d'/' -f5 | cut -d'-' -f1-3)
+#			echo "I cut: "$fdate " from "$file   #test
+			if [[ $(echo $fdate) == $(echo $sdate) || $(echo $fdate) > $(echo $sdate) ]]
 			then
+#				echo "Pulling hours from: "$file  #test
 				cat $file | awk -F "|" '/ENGAGING/ {print $4;}' >> ~/Documents/temphours
 				cat $file | awk -F "|" '/C3DDB/ {print $4;}' >> ~/Documents/temphours
 			fi
 		fi
 	done
+	echo ""
+	echo "Total hours submitted for ENGAGING/C3DDB"
+	echo "========================================"
+	echo ""
 
 else  #If other host, use personal records
 	dir=~/Documents/Hours-$(date +"%Y")/$(date +"%m")/
@@ -41,19 +44,24 @@ else  #If other host, use personal records
 			fdate=$(echo $file | cut -d'/' -f7 | cut -d'-' -f1-3)
 			if [[ $(echo $fdate) == $(echo $sdate)  ||  $(echo $fdate) > $(echo $sdate)  &&  $(echo $fdate) < $(echo $edate) ]]
 			then
+#				echo $file  #test
 				cat $file | awk -F "|" '/ENGAGING/ {print $4;}' >> ~/Documents/temphours
 				cat $file | awk -F "|" '/C3DDB/ {print $4;}' >> ~/Documents/temphours
 			fi
 		done
 	else
 		for file in $dir*; do
-			if [ $(echo $fdate) == $(echo $sdate) ] || [ $(echo $fdate) > $(echo $sdate) ]
+			if [[ $(echo $fdate) == $(echo $sdate) || $(echo $fdate) > $(echo $sdate) ]]
 			then
 				cat $file | awk -F "|" '/ENGAGING/ {print $4;}' >> ~/Documents/temphours
 				cat $file | awk -F "|" '/C3DDB/ {print $4;}' >> ~/Documents/temphours
 			fi
 		done
 	fi
+	echo ""
+	echo "  Total hours worked on ENGAGING/C3DDB"
+	echo "========================================"
+	echo ""
 fi
 
 #cat ~/Documents/temphours  #test
@@ -61,4 +69,5 @@ sed '/^$/d' ~/Documents/temphours > ~/Documents/temphours2
 #echo ""   #test
 #cat ~/Documents/temphours2  #test
 echo "               "$(paste -sd+ ~/Documents/temphours2 | bc)
+echo ""
 rm ~/Documents/temphours*
