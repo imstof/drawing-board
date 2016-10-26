d4f62d4e3d469b0764b9cb493eeb10acc416ebd4
imstof <cehnstrom@techsquare.com>
2016-10-26 10:45:59 -0400

    tweak

--- rackTabPopulate.sh
+++ rackTabPopulate.sh
@@ -4,7 +4,7 @@
 
 #prompt for input
 #assume all items are for same pool to save typing input
-read -p "What cluster are you adding devices to? " dPp
+read -p "What cluster (dPp) are you adding devices to? " dPp
 
 yn="y"
 while [[ $yn == "y" || $yn == "Y" ]]
@@ -19,19 +19,21 @@ do
 	yn2="y"
 	while [[ $yn2 == "y" || $yn2 == "Y" ]]
 	do
-		read -p "Device Name? " dId
+		read -p "Device Name (dId)? " dId
 		read -p "Lower U? " lo
 		read -p "Upper U? " hi
-		read -p "Device Type? " dT
-		read -p "Group? " dPg
+		read -p "Device Type (dT)? " dT
+		read -p "Group (dPg)? " dPg
 #ADD BITS TO VALIDATE INPUT?
 
 #append line to file
 		echo "{'dId':'$dId',   'r':$row,'p':'$pod','c':$cab,'lo':$lo,'hi':$hi,'dT':'$dT',    'dPp':'$dPp','dPg':'$dPg' }," >> ~/Documents/temprackfile.txt
-		read -p "Add another device in this cabinet? " yn2
+		read -p "Add another device to this cabinet? " yn2
 		yn2=${yn2:0:1}
 	done
 
 	read -p "Add another device to this cluster? " yn
 	yn=${yn:0:1}
 done
+
+echo ">>  Formatted data is located in ~/Documents/temprackfile.txt <<"
