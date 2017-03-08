38e72dc8cb6996b9b76c2e6b39a2aad0f6d0d024
imstof <cehnstrom@techsquare.com>
2017-03-08 18:07:33 -0500

    projSearch

--- projSearch.sh
+++ projSearch.sh
@@ -26,20 +26,20 @@ syear=$(echo $sdate | cut -d'-' -f1)
 eyear=$(echo $edate | cut -d'-' -f1)
 smonth=$(echo $sdate | cut -d'-' -f2)
 emonth=$(echo $edate | cut -d'-' -f2)
-echo "sdate : "$sdate	 #test
-echo "edate : "$edate	 #test
-echo "smonth :"$smonth   #test
-echo "emonth :"$emonth   #test
-echo "syear :"$syear 	 #test
-echo "eyear :"$eyear 	 #test
+# echo "sdate : "$sdate	 #test
+# echo "edate : "$edate	 #test
+# echo "smonth :"$smonth   #test
+# echo "emonth :"$emonth   #test
+# echo "syear :"$syear 	 #test
+# echo "eyear :"$eyear 	 #test
 
 		ydir=~/Documents/Hours-$syear/
-		echo "ydir: "$ydir			#test
+# 		echo "ydir: "$ydir			#test
 		smonth=$smonth
-		echo "smonth: "$smonth			#test
+# 		echo "smonth: "$smonth			#test
 # search directories
 
-echo "command run on: "$(date +"%Y-%m-%d %T") >> ~/Documents/temphfiles	#test
+# echo "command run on: "$(date +"%Y-%m-%d %T") >> ~/Documents/temphfiles	#test
 
 # if starting and ending in differnet years, check all files from start date to 12/31
 	while [[ $syear -le $eyear ]]
@@ -47,15 +47,15 @@ echo "command run on: "$(date +"%Y-%m-%d %T") >> ~/Documents/temphfiles	#test
 		if [[ $syear -lt $eyear ]]
 		then
 			ydir=~/Documents/Hours-$syear/
-			echo "ydir: "$ydir			#test
+# 			echo "ydir: "$ydir			#test
 
 			while [[ $((10#$smonth)) -lt "13" ]]
 			do
 				fdir=$ydir$smonth
-				echo "fdir: "$fdir			#test
+# 				echo "fdir: "$fdir			#test
 				for file in $fdir/*
 				do
-					echo "file: "$file >> ~/Documents/temphfiles			#test
+# 					echo "file: "$file >> ~/Documents/temphfiles			#test
 					fdate=$(echo $file | cut -d'/' -f7 | cut -d'-' -f1-3)
 					if [[ $fdate == $sdate || $fdate > $sdate ]]
 					then
@@ -70,15 +70,15 @@ echo "command run on: "$(date +"%Y-%m-%d %T") >> ~/Documents/temphfiles	#test
 		elif [[ $syear == $eyear ]]
 		then
 			ydir=~/Documents/Hours-$syear/
-			echo "ydir: "$ydir			#test
+# 			echo "ydir: "$ydir			#test
 
 			while [[ $((10#$smonth)) -le $emonth ]]
 			do
 				fdir=$ydir$smonth
-				echo "fdir: "$fdir			#test
+# 				echo "fdir: "$fdir			#test
 				for file in $fdir/*
 				do
-					echo "file: "$file >> ~/Documents/temphfiles			#test
+# 					echo "file: "$file >> ~/Documents/temphfiles			#test
 					fdate=$(echo $file | cut -d'/' -f7 | cut -d'-' -f1-3)
 					if [[ $fdate == $sdate || $fdate > $sdate ]]
 					then
@@ -89,7 +89,6 @@ echo "command run on: "$(date +"%Y-%m-%d %T") >> ~/Documents/temphfiles	#test
 			done
 			((syear++))
 		fi
-
 
 	done
 
