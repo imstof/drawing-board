4b9d5fda8b63dd6b6fbe0cd77306739836696636
imstof <cehnstrom@techsquare.com>
2017-03-24 17:01:54 -0400

    swap values desk/lap

--- tblapsync.sh
+++ tblapsync.sh
@@ -2,16 +2,25 @@
 
 # back up then sync profile folders (and not break anything)
 
-#kill tbird
-pidkill thunderbird
-#back up .msf extentions on both hosts
-	if rsync -hav /home/imstof/.thunderbird/3jg4e0zn.default/*.msf /home/imstof/.thunderbird/3jg4e0zn.default.olde/ && rsync -uzhave ssh 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default/.msf 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default.olde/
+#kill tbird (no cli way to do this gracefully?)
+read -p "Have you gracefully shutdown TBird? " yn
+if [ $yn == "y" ]
+then
+	#back up .msf extentions on both hosts
+	if rsync -hav --include="*/" --include="prefs.js" --include="*.msf" --exclude="*" --progress /home/imstof/.thunderbird/3jg4e0zn.default/ /home/imstof/.thunderbird/3jg4e0zn.default.olde/ && rsync -e ssh -uzhav --include="*/" --include="*.msf" --exclude="*" --progress 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default/ /home/imstof/.thunderbird/lap.mv2ohpvu.default.olde/
 	then
-#	#sync .msf up, then down
-		if rsync -uzhave ssh /home/imstof/.thunderbird/3jg4e0zn.default/*.msf 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default/
+	echo "$(tput setaf 2)step 1$(tput sgr 0)"		#test
+	#	#sync .msf up, then down
+		if rsync -e ssh -uzhav --include="*/" --include="prefs.js" --include="*.msf" --exclude="*" --progress /home/imstof/.thunderbird/3jg4e0zn.default/ 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default/
 		then
-			rsync -uzhave ssh 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default//home/imstof/.thunderbird/3jg4e0zn.default/
+	echo "$(tput setaf 2)step 2$(tput sgr 0)"		#test
+		rsync -e ssh -uzhav --include="*/" --include="prefs.js" --include="*.msf" --exclude="*" --progress 192.168.13.30:/home/imstof/.thunderbird/mv2ohpvu.default/ /home/imstof/.thunderbird/3jg4e0zn.default/ 
 		fi
 	fi
-#start tbird
-thunderbird
+	#start tbird
+	thunderbird &
+	exit 0
+else
+	echo "Please gracefully shutdown TBird and rerun."
+	exit 1
+fi
