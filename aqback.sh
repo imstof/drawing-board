4f43a69d9b2bbcbab2519714d916fc9097d13a5d
imstof <cehnstrom@techsquare.com>
2017-07-07 16:39:04 -0400

    aqback tweak

--- aqback.sh
+++ aqback.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 #Script to back up home dir to aquinas
 
-rsync -uzhave ssh --progress --exclude-from='/home/imstof/exclude_from_backup.txt' /home/imstof cehnstrom@aquinas.techsquare.com:~/[BACKUP-FOLDER]
+#use -n for dry-run!!
+rsync -nuzhave ssh --progress --exclude-from='/home/imstof/exclude_from_backup.txt' /home/imstof cehnstrom@aquinas.techsquare.com:~/[BACKUP-FOLDER] --delete
+
+read -p "DRY-RUN complete.\nType $(tput setaf 2)yes$(tput sgr 0) to execute the rsync? " yn
+if [[ $yn == "yes" ]]
+then
+	rsync -nuzhave ssh --progress --exclude-from='/home/imstof/exclude_from_backup.txt' /home/imstof cehnstrom@aquinas.techsquare.com:~/[BACKUP-FOLDER] --delete
+	echo "$(tput setaf 2)rsync executed$(tput sgr 0)"
+	exit 0
+else
+	echo "$(tput setaf 1)rsync aborted!!$(tput sgr 0)"
+	exit 0
+fi
