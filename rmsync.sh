25cb83a205a94b67d57183eef4549fd2f8aa958a
imstof <cehnstrom@techsquare.com>
2017-07-11 11:57:17 -0400

    rm from all script

---
+++ rmsync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#script to delete file from Desktop, Laptop, and Aquinas
+#from Desktop
+
+echo "deleting from aquinas..."
+ssh cehnstrom@aquinas.techsquare.com rm $(echo $1 | sed 's/imstof/cehnstrom/')
+echo "deleting from KLaptop..."
+ssh 192.168.13.29 rm $1
+echo "deleting local..."
+rm $1
