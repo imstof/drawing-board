939fa2170767e4ad7d9d1d866cc0b25452a7a934
imstof <cehnstrom@techsquare.com>
2017-07-18 16:04:36 -0400

    tweak

--- vcheck.sh
+++ vcheck.sh
@@ -2,4 +2,11 @@
 
 #quick check of contract hours
 
-cat $1 | sed '/HOME/d' | sed '/LUNCH/d' | cut -d'|' -f4 | sed '/^$/d' | paste -sd+ | bc
+if [[ -z $1 ]]
+then
+	NOW=$(date +"%Y-%m-%d")
+else
+	NOW=$1
+fi
+
+cat ~/Documents/$NOW-cehnstrom | sed '/HOME/d' | sed '/LUNCH/d' | cut -d'|' -f4 | sed '/^$/d' | paste -sd+ | bc
