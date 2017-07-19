e93b9181c4f42b663d1abc696d7ed4c67ff98784
imstof <cehnstrom@techsquare.com>
2017-07-19 09:05:23 -0400

    rmsyncl loop

--- rmsyncl.sh
+++ rmsyncl.sh
@@ -3,46 +3,54 @@
 #script to delete file from Desktop, Laptop, and Aquinas
 #from Desktop
 
-echo "deleting local..."
-if rm $1
-then
-	echo "...deleted"
-else
-	echo "...failed"
-fi
-echo "deleting from aquinas..."
-if ssh cehnstrom@aquinas.techsquare.com rm $(echo $1 | sed 's/imstof/cehnstrom/')
-then
-	echo "...deleted"
-else
-	echo "...failed"
-fi
-
-# check if desktop is connected. if not, add script to crontab to keep trying
-echo "deleting from Desktop-MGHPCC..."
-if [[ -n $(ping -c1 192.168.13.30 | grep ' 0% packet loss') ]]
-then
-	if ssh 192.168.13.30 rm $1
+#new version to expand dir/*
+for file in $1
+do
+	echo $file
+	echo
+	echo "deleting local..."
+	if rm $file
 	then
 		echo "...deleted"
+	else
+		echo "...failed"
 	fi
-else
-	echo "...failed"
-	echo "adding job to crontab..."
-	if [[ -z $(crontab -l) ]]
+	echo "deleting from aquinas..."
+	if ssh cehnstrom@aquinas.techsquare.com rm $(echo $file | sed 's/imstof/cehnstrom/')
 	then
-		if echo "*/1 7-18 * * 1-5 /home/imstof/bin/rmdeskcron $1" | crontab -
+		echo "...deleted"
+	else
+		echo "...failed"
+	fi
+
+	# check if desktop is connected. if not, add script to crontab to keep trying
+	echo "deleting from Desktop-MGHPCC..."
+	if [[ -n $(ping -c1 192.168.13.30 | grep ' 0% packet loss') ]]
+	then
+		if ssh 192.168.13.30 rm $file
 		then
-			echo "...file will be deleted when Desktop-MGHPCC is present"
-		else
-			echo "...failed"
+			echo "...deleted"
 		fi
 	else
-		if crontab -l | sed "$ a\*\/1 7-18 * * 1-5 \/home\/imstof\/bin\/rmdeskcron $1" | crontab -
+		echo "...failed"
+		echo "adding job to crontab..."
+#sed won't '$ a' to empty crontab, echo the single line
+		if [[ -z $(crontab -l) ]]
 		then
-			echo "...file will be deleted when Desktop-MGHPCC is present"
+			if echo "*/1 7-18 * * 1-5 /home/imstof/bin/rmdeskcron $file" | crontab -
+			then
+				echo "...file will be deleted when Desktop-MGHPCC is present"
+			else
+				echo "...failed"
+			fi
 		else
-			echo "...failed"
+			if crontab -l | sed "$ a\*\/1 7-18 * * 1-5 \/home\/imstof\/bin\/rmdeskcron $file" | crontab -
+			then
+				echo "...file will be deleted when Desktop-MGHPCC is present"
+			else
+				echo "...failed"
+			fi
 		fi
 	fi
-fi
+	echo
+done
