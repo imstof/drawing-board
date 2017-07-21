6a77275d2f1f0ae51b91bffe90edfbfa15fa6ce0
imstof <cehnstrom@techsquare.com>
2017-07-21 15:05:52 -0400

    tweak rmsyncl for dirs

---
+++ wip-rmdeskcron.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# used by 'rmsyncl' to rm file from Desktop-MGHPCC when it becomes available then delete it's crontab on success
+
+if [[ -d $1 ]]
+then	
+	if [[ -n $(ping -c1 192.168.13.30 | grep ' 0% packet loss') ]]
+	then
+		if ssh -i /home/imstof/.ssh/id_rsa_lapdesk 192.168.13.30 rm -r $1
+		then
+			echo $1 "deleted from Desktop-MGHPCC" | mail -s "$1 deleted" cehnstrom@techsquare.com
+			file=$(echo $1 | sed 's:\/:\\\/:g')
+			crontab -l | sed "/rmdeskcron\ $file/d" | crontab -
+		fi
+	fi
+else
+	if [[ -n $(ping -c1 192.168.13.30 | grep ' 0% packet loss') ]]
+	then
+		if ssh -i /home/imstof/.ssh/id_rsa_lapdesk 192.168.13.30 rm $1
+		then
+			echo $1 "deleted from Desktop-MGHPCC" | mail -s "$1 deleted" cehnstrom@techsquare.com
+			file=$(echo $1 | sed 's:\/:\\\/:g')
+			crontab -l | sed "/rmdeskcron\ $file/d" | crontab -
+		fi
+	fi
+fi
