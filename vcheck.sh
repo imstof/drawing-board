4dd59769115c7d8ac04e791ddd962cb2cf827c74
Christophe Ehnstrom <imstof@KLaptop-TS>
2017-07-22 08:19:27 -0400

    dlkh

--- vcheck.sh
+++ vcheck.sh
@@ -9,9 +9,4 @@ else
 	NOW=$1
 fi
 
-echo
-echo "Hours|Project|Notes"
-cat ~/Documents/$NOW-cehnstrom | sed '/HOME/d' | sed '/LUNCH/d' | cut -d'|' -f4,5,8 | awk -F'|' '$1!=""'
-
-echo
-echo "Total: "$(cat ~/Documents/$NOW-cehnstrom | sed '/HOME/d' | sed '/LUNCH/d' | cut -d'|' -f4 | sed '/^$/d' | paste -sd+ | bc)
+cat ~/Documents/$NOW-cehnstrom | sed '/HOME/d' | sed '/LUNCH/d' | cut -d'|' -f4 | sed '/^$/d' | paste -sd+ | bc
