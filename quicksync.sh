9e3a8bf3370b54259d2593e4714c735129f3a8bd
imstof <cehnstrom@techsquare.com>
2018-02-14 12:58:51 -0500

    sdl;kh

--- quicksync.sh
+++ quicksync.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Just update doc files
-
-#Down
-rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
+#and slack logs
 
 #Up
 rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
+rsync -uzhave ssh --progress ~/.purple/logs/jabber/imstof@t3chsquar3.xmpp.slack.com/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
+
+#Down
+rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
+#rsync -uzhave ssh --progress cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/ ~/Documents/.slacklogs
