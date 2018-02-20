77d5db3c19f5efceebd80a919d7a6be627e71eb8
Christophe Ehnstrom <cehnstrom@techsquare.com>
2018-02-20 08:35:25 -0500

    a;slfkdj

--- quicksync.sh
+++ quicksync.sh
@@ -4,7 +4,7 @@
 
 #Up
 rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
-rsync -uzhave ssh --progress ~/.purple/logs/jabber/imstof@t3chsquar3.xmpp.slack.com/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
+#rsync -uzhave ssh --progress ~/.purple/logs/jabber/imstof@t3chsquar3.xmpp.slack.com/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
 
 #Down
 rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
