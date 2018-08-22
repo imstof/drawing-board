2cf0290f8ff8f382d3579ce89312528e9f248bf5
Christophe Ehnstrom <imstof@FLaptopK0.localdomain>
2018-08-22 07:38:22 -0400

    alsd

--- hours.sh
+++ hours.sh
@@ -43,7 +43,7 @@ fi
 file=~/Documents/$now-cehnstrom
 
 echo "cehnstrom|$now $stime|$now ||TS|Y|N|email,misc" >> $file
-echo "cehnstrom|$now $stime|$now ||HOME|Y|N|domestic duties" >> $file
+echo "cehnstrom|$now |$now ||HOME|Y|N|domestic duties" >> $file
 echo "cehnstrom|$now |$now |0.75|HOME|Y|N|to mghpcc" >> $file
 echo "cehnstrom|$now |$now |0.25|HOLYOKE|Y|N|walkthrough" >> $file
 echo "cehnstrom|$now |$now ||TS|Y|N|email,issues" >> $file
