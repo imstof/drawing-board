9825910da69500d3883ee100ff25c69cfe58e297
Christophe Ehnstrom <cehnstrom@techsquare.com>
2019-03-15 10:25:24 -0400

    Fri Mar 15 10:25:24 EDT 2019

---
+++ dammit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+gethours(){
+	echo $([ -n "$(grep $1 $2 | awk -F'|' '$4 {printf $4"+"}' | sed 's/+$//' | paste | bc)" ] && grep $1 $2 | awk -F'|' '$4 {printf $4"+"}' | sed 's/+$//' | paste | bc || echo 0)
+}
+
+#file0=/home/imstof/Documents/2019-03-12-cehnstrom
+#file1=/home/imstof/Documents/Hours-2019/03/2019-03-12-cehnstrom
+
+#[[ ! -e $file0 ]] && echo yay || echo $file1
+
+sdate=20190312
+edate=20190313
+
+for tmpdate in $(seq $sdate $edate)
+do
+	file0=/home/imstof/Documents/$(date -d $tmpdate +%Y-%m-%d)-cehnstrom
+	file1=/home/imstof/Documents/Hours-$(date -d $tmpdate +%Y)/$(date -d $tmpdate +%m)/$(date -d $tmpdate +%Y-%m-%d)-cehnstrom
+	[[ -e $file0 ]] && echo yay || echo $file1
+done
