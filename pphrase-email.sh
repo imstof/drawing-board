3fa655041cd1eebd3c8ef7957750b024b5ebd917
imstof <cehnstrom@techsquare.com>
2019-04-05 10:59:03 -0400

    Fri Apr  5 10:59:03 EDT 2019

---
+++ pphrase-email.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for user in $@
+do
+	echo -e "\n\nHello $user,\n\nIf you've forgtten the passphrase for your key you can generate a new pair with\n\nssh-keygen -t rsa\n\nand send us the contents of the public portion. We'll put that in place and you'll be able to log in again.\nCheers,\nChristophe"
+done
