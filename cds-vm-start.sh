52ae7f437def70c9a78109b6210bb2b0f4483a82
imstof <cehnstrom@techsqure.com>
2019-04-10 21:26:39 -0400

    Wed Apr 10 21:26:39 EDT 2019

--- cds-vm-start.sh
+++ cds-vm-start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 virsh start CentOS-7dot6-VM
-echo waiting 15 seconds for boot...
-sleep 15
-echo connecting...
-ssh -lroot -i /root/.ssh/id_rsa_lapdesk 192.168.122.76
+echo waiting to connect...
+sleep 11
+echo trying to connect...
+until ssh -lroot -i /root/.ssh/id_rsa_lapdesk 192.168.122.76
+do
+	sleep 1
+done
 virsh shutdown CentOS-7dot6-VM
