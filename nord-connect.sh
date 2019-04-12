9fa429517cd16c00ed63220ad8bba48d4ce24da2
imstof <cehnstrom@techsqure.com>
2019-04-11 23:25:19 -0400

    Thu Apr 11 23:25:19 EDT 2019

--- nord-connect.sh
+++ nord-connect.sh
@@ -4,4 +4,12 @@
 # MUST BE SUDO'd
 
 #check for existing config file
+[[ -e /etc/openvpn/ovpn_udp/us$1.nordvpn.com.udp.ovpn ]] || wget https://downloads.nordcdn.com/configs/files/ovpn_udp/servers/us$1.nordvpn.com.udp.ovpn -O /etc/openvpn/ovpn_udp/us$1.nordvpn.com.udp.ovpn
 
+#pull ip && port from config file
+#regex to get "remote followed by space + number" for ip/port fields
+NORDIP=
+NORDPORT=
+
+#sed /etc/NetworkManager/system-connections/nord-vpn to change ip/port
+#that should work?
