686a5f57880c18b9cd8d9ba1dab7047fab1e3bd3
imstof <cehnstrom@techsquare.com>
2019-05-22 10:36:29 -0400

    Wed May 22 10:36:29 EDT 2019

--- quicksync.sh
+++ quicksync.sh
@@ -4,8 +4,10 @@
 
 #Up
 rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
-#rsync -uzhave ssh --progress ~/.purple/logs/jabber/imstof@t3chsquar3.xmpp.slack.com/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
+#only run this UP from desktop
+#rsync -uzhave ssh --progress ~/.weechat/logs/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
 
 #Down
 rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
-#rsync -uzhave ssh --progress cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/ ~/Documents/.slacklogs
+#only run this DOWN from lap distros
+#rsync -uzhave ssh --progress cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/ ~/Documents/.slacklogs/
