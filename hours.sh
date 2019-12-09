5675473ef247361f98cfaf9d8b8631ad7a960a25
imstof <cehnstrom@techsquare.com>
2019-12-09 11:30:00 -0500

    Mon Dec  9 11:30:00 EST 2019

--- hours.sh
+++ hours.sh
@@ -49,10 +49,10 @@ echo "cehnstrom|$now |$now |0.25|HOLYOKE|Y|N|walkthrough" >> $file
 echo "cehnstrom|$now |$now ||TS|Y|N|email,misc" >> $file
 echo "cehnstrom|$now |$now ||ENGAGING|Y|N|" >> $file
 echo "cehnstrom|$now |$now ||C3DDB|Y|N|" >> $file
+echo "cehnstrom|$now |$now ||HPC/HELP|Y|N|c3ddb_user_help" >> $file
 echo "cehnstrom|$now |$now ||HOLYOKE|Y|N|" >> $file
 #echo "cehnstrom|$now |$now ||NEURO|Y|N|" >> $file
 #echo "cehnstrom|$now |$now ||SPHHS|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||HPC/HELP|Y|N|c3ddb_user_help" >> $file
 echo "cehnstrom|$now |$now ||LUNCH|Y|N|lunch" >> $file
 echo "cehnstrom|$now |$now |||Y|N|" >> $file
 
