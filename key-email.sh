bc03d5fc743b964c1952ac7d82dc03e5b06846a3
Christophe Ehnstrom <cehnstrom@techsquare.com>
2020-01-07 09:25:05 -0500

    Tue Jan  7 09:25:05 EST 2020

--- key-email.sh
+++ key-email.sh
@@ -2,5 +2,5 @@
 
 for user in $@
 do
-	echo -e "\n\nHello $user,\n\nThere are no passwords on the cluster, where all authorization is via ssh key. If you've forgotten the passphrase to your key you can generate a new pair with\n\nssh-keygen -t rsa\n\nand send us the contents of the public portion. We'll put that in place and you will be able to log in again.\n\nCheers,\nChristophe\n"
+	echo -e "\n\nHello $user,\n\nThere are no passwords on the cluster, where all authentication is via ssh key. If you've forgotten the passphrase to your key you can generate a new pair with\n\nssh-keygen -t rsa\n\nand send us the contents of the public portion. We'll put that in place and you will be able to log in again.\n\nCheers,\nChristophe\n"
 done
