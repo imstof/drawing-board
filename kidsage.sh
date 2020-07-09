d4fca55fcfe1ce8ded9e015f1726bcbae03b6737
Christophe Ehnstrom <cehnstrom@techsquare.com>
2020-07-09 16:29:28 -0400

    Thu Jul  9 16:29:28 EDT 2020

--- kidsage.sh
+++ kidsage.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+#doy minus zeroes
+TODAY=$(date +%j)
+while [[ ${TODAY:0:1} = 0 ]];do TODAY=${TODAY:1};done
+#doy birthdays checked for leapyear && minus 0s
+#isabel
+[[ $(date -d 6/24 +%j) -ge $TODAY ]] && IBDAY=$(date -d 6/24 +%j) || IBDAY=$(date -d 6/24/$(date -d "next year" +%y) +%j)
+#theo
+M25=$(date -d 3/25 +%j);while [[ ${M25:0:1} = 0 ]];do M25=${M25:1};done
+#echo $M25			#TEST
+[[ $M25 -ge $TODAY ]] && TBDAY=$M25 || TBDAY=$(date -d 3/25/$(date -d "next year" +%y) +%j);while [[ ${TBDAY:0:1} = 0 ]];do TBDAY=${TBDAY:1};done
 
-jyear=$(if [[ $(date +"%m%d") - 
-#bsecs=$(date --date="06/24/2012 20:31" +"%s")
-bsecs=$(date --date="06/24/$jyear 20:31" +"%s")
-nsecs=$(date +"%s")
+echo TODAY=$TODAY		#TEST
+echo IBDAY=$IBDAY		#TEST
+echo TBDAY=$TBDAY		#TEST
 
-jsecs=($nsecs - $bsecs)
 
-echo $bsecs		#test
