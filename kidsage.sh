6d1f6c423d7579cfbd648ed00a230536e022a9e2
Christophe Ehnstrom <cehnstrom@techsquare.com>
2020-07-24 14:59:11 -0400

    Fri Jul 24 14:59:11 EDT 2020

--- kidsage.sh
+++ kidsage.sh
@@ -3,7 +3,9 @@
 #doy minus zeroes
 TODAY=$(date +%j)
 while [[ ${TODAY:0:1} = 0 ]];do TODAY=${TODAY:1};done
-#doy birthdays checked for leapyear && minus 0s
+#days in year being calculated, checked for leap year
+DYEAR=$(date -d 12/31/$(date -d "next year" +%y) +%j)
+#doy birthdays minus 0s
 #isabel
 [[ $(date -d 6/24 +%j) -ge $TODAY ]] && IBDAY=$(date -d 6/24 +%j) || IBDAY=$(date -d 6/24/$(date -d "next year" +%y) +%j)
 #theo
@@ -11,6 +13,7 @@ M25=$(date -d 3/25 +%j);while [[ ${M25:0:1} = 0 ]];do M25=${M25:1};done
 #echo $M25			#TEST
 [[ $M25 -ge $TODAY ]] && TBDAY=$M25 || TBDAY=$(date -d 3/25/$(date -d "next year" +%y) +%j);while [[ ${TBDAY:0:1} = 0 ]];do TBDAY=${TBDAY:1};done
 
+echo DYEAR=$DYEAR		#TEST
 echo TODAY=$TODAY		#TEST
 echo IBDAY=$IBDAY		#TEST
 echo TBDAY=$TBDAY		#TEST
