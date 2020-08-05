52790811f7a45d360bf5b812467bc5db3ad9daef
Christophe Ehnstrom <cehnstrom@techsquare.com>
2020-08-05 13:12:08 -0400

    Wed Aug  5 13:12:08 EDT 2020

--- quicksync.sh
+++ quicksync.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#Just update doc files
+#Just update doc files on aquinas/aristotle
 #and slack logs
 
 #Up
 rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
+rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aristotle.techsquare.com:~/Documents/
 #only run this UP from desktop
 #rsync -uzhave ssh --progress ~/.weechat/logs/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
 
 #Down
 rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
+rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aristotle.techsquare.com:~/Documents/ ~/Documents/
 #only run this DOWN from lap distros
 #rsync -uzhave ssh --progress cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/ ~/Documents/.slacklogs/
