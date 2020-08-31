0e6f8c53286d4d52aef63d19d9ae842aa32ccb37
imstof <cehnstrom@techsquare.com>
2020-08-31 06:03:31 -0400

    Mon Aug 31 06:03:31 EDT 2020

--- quicksync.sh
+++ quicksync.sh
@@ -3,13 +3,13 @@
 #and slack logs
 
 #Up
-rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
+#rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
 rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aristotle.techsquare.com:~/Documents/
 #only run this UP from desktop
 #rsync -uzhave ssh --progress ~/.weechat/logs/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
 
 #Down
-rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
+#rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aquinas.techsquare.com:~/Documents/ ~/Documents/
 rsync -uzhave ssh --exclude '.swp' --progress cehnstrom@aristotle.techsquare.com:~/Documents/ ~/Documents/
 #only run this DOWN from lap distros
 #rsync -uzhave ssh --progress cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/ ~/Documents/.slacklogs/
