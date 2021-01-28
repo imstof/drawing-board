b2f9dca4f2be72873c4db3319316cf9b86128a32
imstof <cehnstrom@techsquare.com>
2021-01-28 10:39:02 -0500

    Thu Jan 28 10:39:02 AM EST 2021

--- quicksync.sh
+++ quicksync.sh
@@ -5,8 +5,8 @@
 #Up
 #first clean up accounting ledger
 ledger_saves
-#rsync -uzhave ssh --exclude-from-from='/home/imstof/exclude-from-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
-rsync -uzhave ssh --exclude-from-from='/home/imstof/exclude-from-from-sync.txt' --progress ~/Documents/ cehnstrom@aristotle.techsquare.com:~/Documents/
+#rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aquinas.techsquare.com:~/Documents/
+rsync -uzhave ssh --exclude-from='/home/imstof/exclude-from-sync.txt' --progress ~/Documents/ cehnstrom@aristotle.techsquare.com:~/Documents/
 rsync -uzhave ssh --progress ~/.weechat/logs/ cehnstrom@aristotle.techsquare.com:~/weechat-logs/
 #only run this UP from desktop
 #rsync -uzhave ssh --progress ~/.weechat/logs/ cehnstrom@aquinas.techsquare.com:~/Documents/.slacklogs/
