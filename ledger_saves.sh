84132e024edaa4fb06de78f7de960901c75dd7ad
imstof <cehnstrom@techsquare.com>
2021-02-01 09:11:49 -0500

    Mon Feb  1 09:11:49 AM EST 2021

--- ledger_saves.sh
+++ ledger_saves.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find /home/imstof/Documents/personal/gnucash_books -maxdepth 1 -type f -name 'Ehnstrom_Ledger.gnucash.*.*g*' | xargs tar -cvzf /home/imstof/Documents/personal/gnucash_books/archive/ledgers-$(date +%Y%m%d-%H%M).tgz 2>/dev/null
+find /home/imstof/Documents/personal/gnucash_books -maxdepth 1 -type f -name '*Ehnstrom*.gnucash.*.*g*' | xargs tar -cvzf /home/imstof/Documents/personal/gnucash_books/archive/ledgers-$(date +%Y%m%d-%H%M).tgz 2>/dev/null
 
-find /home/imstof/Documents/personal/gnucash_books -maxdepth 1 -type f -name 'Ehnstrom_Ledger.gnucash.*.*g*' -exec rm {} \;
+find /home/imstof/Documents/personal/gnucash_books -maxdepth 1 -type f -name '*Ehnstrom*.gnucash.*.*g*' -exec rm {} \;
