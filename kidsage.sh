4b1e208cc1de87ecb0cfa9bbef71e5616df1711c
imstof <cehnstrom@techsquare.com>
2021-02-02 13:46:32 -0500

    Tue Feb  2 01:46:32 PM EST 2021

--- kidsage.sh
+++ kidsage.sh
@@ -18,20 +18,20 @@ M25=$(date -d 3/25 +%j);while [[ ${M25:0:1} = 0 ]];do M25=${M25:1};done
 #echo $M25			#TEST
 [[ $M25 -ge $TODAY ]] && TBDAY=$M25 && TYEARS=$(echo $(date -d "last year" +%Y)-2015 | bc) || (TBDAY=$(date -d 3/25/$(date -d "next year" +%y) +%j) && TYEARS=$(echo $(date +%Y)-2015 | bc ));while [[ ${TBDAY:0:1} = 0 ]];do TBDAY=${TBDAY:1};done
 
-echo LYEAR=$LYEAR		#TEST
-echo TYEAR=$TYEAR		#TEST
-echo NYEAR=$NYEAR		#TEST
-echo TODAY=$TODAY		#TEST
-echo IBDAY=$IBDAY		#TEST
-echo IYEARS=$IYEARS		#TEST
-echo TBDAY=$TBDAY		#TEST
-echo TYEARS=$TYEARS		#TEST
+#echo LYEAR=$LYEAR		#TEST
+#echo TYEAR=$TYEAR		#TEST
+#echo NYEAR=$NYEAR		#TEST
+#echo TODAY=$TODAY		#TEST
+#echo IBDAY=$IBDAY		#TEST
+#echo IYEARS=$IYEARS		#TEST
+#echo TBDAY=$TBDAY		#TEST
+#echo TYEARS=$TYEARS		#TEST
 
 #calculate
 #isabel
 [[ $TODAY -le $(date -d 6/24 +%j) ]] && IAGE=$(echo $IYEARS+\(\(190+$TODAY\)/$TYEAR\) | bc -l ) || IAGE=$(echo $IYEARS+\(\($TODAY-$IBDAY\)/$TYEAR\) | bc -l )
 
-[[ $TODAY -le $M25 ]] && TAGE=$(echo $TYEARS+\(\(281+$TODAY\)/$TYEAR\) | bc -l ) || TAGE=$(echo $TYEARS+\(\($TODAY-$TBDAY\)/$TYEAR\) | bc -l )
+[[ $TODAY -le $M25 ]] && TAGE=$(echo $TYEARS+\(\(281+$TODAY\)/$TYEAR\) | bc -l) || TAGE=$(echo $TYEARS+\(\($TODAY-$TBDAY\)/$TYEAR\) | bc -l )
 
 #report
 echo Isabel is $IAGE years old
