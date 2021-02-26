f7cc217c2cc401d01e99fc29eb9a0be5e361bc86
imstof <cehnstrom@techsquare.com>
2021-02-26 13:27:49 -0500

    Fri Feb 26 01:27:49 PM EST 2021

---
+++ newgroup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello $1,"
+echo
+echo "You've been added to the $2 group. You'll need to log out of any active sessions as *nix groups are only assigned at login."
+echo
+echo "Best,"
+echo "Christophe && c3ddb-admin"
