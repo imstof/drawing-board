921f16a09be0ef7ec584d4f77033469a6f63a3b1
imstof <cehnstrom@techsquare.com>
2021-03-03 12:53:08 -0500

    Wed Mar  3 12:53:08 PM EST 2021

--- hours.sh
+++ hours.sh
@@ -24,52 +24,70 @@ else
 	stime=$1
 fi
 
+#Fill out morning care&&feeding bits
+incdate=$(date -d $stime +%s)
+((incdate=incdate+900))
+stime15=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime30=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime45=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime60=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime75=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime90=$(date -d @$incdate +%H:%M)
+((incdate=incdate+900))
+stime105=$(date -d @$incdate +%H:%M)
+
+
 #If no date given, current date. else $2
-if [ -z "$2" ]
-then
-	now=$(date +"%Y-%m-%d")
-else
-	now=$(date --date=$2 +"%Y-%m-%d")
-fi
-
-if [ -e ~/Documents/$now-cehnstrom ]
-then
-	echo ""
-	echo "File: ~/Documents/"$now"-cehnstrom already exists."
-	echo ""
-	exit 0
-fi
-
-file=~/Documents/$now-cehnstrom
-
-echo "cehnstrom|$now $stime|$now |0.25|TS|Y|N|email,misc" >> $file
-echo "cehnstrom|$now |$now |0.25|C3DDB|Y|N|care&&feeding" >> $file
-echo "cehnstrom|$now |$now |0.25|ENGAGING|Y|N|care&&feeding" >> $file
-echo "cehnstrom|$now |$now |0.25|SATORI|Y|N|care&&feeding" >> $file
-echo "cehnstrom|$now |$now ||HOME|Y|N|domestic_duties" >> $file
-#echo "cehnstrom|$now |$now |0.25|HOLYOKE|Y|N|walkthrough" >> $file
-#echo "cehnstrom|$now |$now |0.25|CDS|Y|N|logs" >> $file
-echo "cehnstrom|$now |$now ||TS|Y|N|email,misc" >> $file
-echo "cehnstrom|$now |$now ||ENGAGING|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||C3DDB|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||HPC/HELP|Y|N|c3ddb_user_help" >> $file
-echo "cehnstrom|$now |$now ||HOLYOKE|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||MIT/EAPS/JP|Y|N|" >> $file
-#echo "cehnstrom|$now |$now ||NEURO|Y|N|" >> $file
-#echo "cehnstrom|$now |$now ||SPHHS|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||SATORI|Y|N|" >> $file
-echo "cehnstrom|$now |$now ||PTO|Y|N|no_notes_req" >> $file
-echo "cehnstrom|$now |$now ||PTO/SICK|Y|N|no_notes_req" >> $file
-echo "cehnstrom|$now |$now ||PTO/DEPENDENT|Y|N|no_notes_req" >> $file
-echo "cehnstrom|$now |$now ||LUNCH|Y|N|lunch" >> $file
-echo "cehnstrom|$now |$now |||Y|N|" >> $file
-
-if [ -z "$1" ]
-then
-	echo "$file generated with start time of $stime."
-	quicksync
-	exit 0
-fi
-vim $file
-quicksync
-
+#if [ -z "$2" ]
+#then
+#	now=$(date +"%Y-%m-%d")
+#else
+#	now=$(date --date=$2 +"%Y-%m-%d")
+#fi
+#
+#if [ -e ~/Documents/$now-cehnstrom ]
+#then
+#	echo ""
+#	echo "File: ~/Documents/"$now"-cehnstrom already exists."
+#	echo ""
+#	exit 0
+#fi
+#
+#file=~/Documents/$now-cehnstrom
+#
+#echo "cehnstrom|$now $stime|$now $stime15|0.25|TS|Y|N|email,misc" >> $file
+#echo "cehnstrom|$now $stime30|$now $stime45|0.25|C3DDB|Y|N|care&&feeding" >> $file
+#echo "cehnstrom|$now $stime60|$now $stime75|0.25|ENGAGING|Y|N|care&&feeding" >> $file
+#echo "cehnstrom|$now $stime90|$now $stime105|0.25|SATORI|Y|N|care&&feeding" >> $file
+#echo "cehnstrom|$now |$now ||HOME|Y|N|domestic_duties" >> $file
+##echo "cehnstrom|$now |$now |0.25|HOLYOKE|Y|N|walkthrough" >> $file
+##echo "cehnstrom|$now |$now |0.25|CDS|Y|N|logs" >> $file
+#echo "cehnstrom|$now |$now ||TS|Y|N|email,misc" >> $file
+#echo "cehnstrom|$now |$now ||ENGAGING|Y|N|" >> $file
+#echo "cehnstrom|$now |$now ||C3DDB|Y|N|" >> $file
+#echo "cehnstrom|$now |$now ||HPC/HELP|Y|N|c3ddb_user_help" >> $file
+#echo "cehnstrom|$now |$now ||HOLYOKE|Y|N|" >> $file
+#echo "cehnstrom|$now |$now ||MIT/EAPS/JP|Y|N|" >> $file
+##echo "cehnstrom|$now |$now ||NEURO|Y|N|" >> $file
+##echo "cehnstrom|$now |$now ||SPHHS|Y|N|" >> $file
+#echo "cehnstrom|$now |$now ||SATORI|Y|N|" >> $file
+#echo "cehnstrom|$now |$now ||PTO|Y|N|no_notes_req" >> $file
+#echo "cehnstrom|$now |$now ||PTO/SICK|Y|N|no_notes_req" >> $file
+#echo "cehnstrom|$now |$now ||PTO/DEPENDENT|Y|N|no_notes_req" >> $file
+#echo "cehnstrom|$now |$now ||LUNCH|Y|N|lunch" >> $file
+#echo "cehnstrom|$now |$now |||Y|N|" >> $file
+#
+#if [ -z "$1" ]
+#then
+#	echo "$file generated with start time of $stime."
+#	quicksync
+#	exit 0
+#fi
+#vim $file
+#quicksync
+#
