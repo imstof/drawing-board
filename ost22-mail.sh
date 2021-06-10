6857502de5453f53aa9d03c96ac4335efaaaf307
imstof <cehnstrom@techsquare.com>
2021-06-10 15:51:55 -0400

    Thu Jun 10 15:51:55 EDT 2021

---
+++ ost22-mail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "You should have received an email informing you that you have a file:
+
+>/home/$1/$1-ost22-files.txt
+
+listing all of your files associated with the recovered OST. All files should be accessible now, but we can not be sure that any individual file was not corrupted by this extreme event."
